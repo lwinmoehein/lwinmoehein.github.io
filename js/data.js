fe3fead8414e311dfa35d5223740e1fb38d4e5e9
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-03-17 03:19:32 +0400

    - add english daily academy project

--- js/data.js
+++ js/data.js
@@ -6,27 +6,63 @@ document.addEventListener('alpine:init', () => {
         projectDetails:[
             {
                 id:'english-daily',
-                title:'English Daily Academy',
+                title:'englishdailyacademy.com',
                 type_id:'web',
-                type_name:'Web Application',
-                demo_url:'',
-                description:'To make the images responsive and scale appropriately, we can set the maximum width of the images to 100% of their parent container, which in this case is .slide. Here\'s the updated SCSS code:\n',
+                type_name:'Web',
+                demo_url:'https://englishdailyacademy.com',
+                short_description:'An English learning web application where students can learn from courses uploaded by website admins.',
+                description:'englishdailyacademy.com is a website for English learners where students can learn English from the website. Courses and contents are uploaded by the website admin using Admin Portal. A student first subscribe to the website by monthly, yearly etc. Course contents contain articles, videos, audios, quizzes and exams to automatically give students course completion certificates directly from the website.',
                 technologies:['Laravel','Vue.js','MySQL','Ubuntu','Hostinger','Cloudflare','GithubActions'],
                 features:[
                     {
-                        name:'Register',
-                        image_url:'',
-                        description:'This is for registering shit'
+                        name:'Browse English Courses',
+                        image_url:'images/english-daily/home.png',
+                        description:'Browse the English courses created by website admin.'
                     },
                     {
-                        name:'Login',
-                        image_url:'',
-                        description:'This is for loggin in shit'
+                        name:'Course Detail',
+                        image_url:'images/english-daily/course_detail.png',
+                        description:'View the course detail by clicking the course.'
                     },
                     {
-                        name:'Test',
-                        image_url:'',
-                        description:'Sar U Yay Ngar'
+                        name:'Course Outline',
+                        image_url:'images/english-daily/course_outline.png',
+                        description:'View the course outline with sections.'
+                    },
+                    {
+                        name:'Course Detail for Article Content',
+                        image_url:'images/english-daily/course_detail_text.png',
+                        description:'View the course\'s article content.'
+                    },
+                    {
+                        name:'Course Detail for Video Content',
+                        image_url:'images/english-daily/course_detail_video.png',
+                        description:'View the course\'s video content.'
+                    },
+                    {
+                        name:'Course Detail for Quizzes',
+                        image_url:'images/english-daily/quizz_question.png',
+                        description:'Students can also learn from quizzes.'
+                    },
+                    {
+                        name:'Account Settings',
+                        image_url:'images/english-daily/settings.png',
+                        description:'Users can change settings and manage their account.'
+                    },
+                    {
+                        name:'Manage Courses - Admin',
+                        image_url:'images/english-daily/admin_dashboard.png',
+                        description:'Manage courses in admin portal ( create,update,delete etc ).'
+                    },
+                    {
+                        name:'Manage Subscription Plans- Admin',
+                        image_url:'images/english-daily/plan_manage.png',
+                        description:'Manage subscription plans in admin portal to configure access to course.'
+                    },
+                    {
+                        name:'Manage Course\'s Quizzes - Admin',
+                        image_url:'images/english-daily/manage_quizzes.png',
+                        description:'Manage course\'s quizzes in admin portal.'
                     }
                 ]
             },
@@ -34,9 +70,10 @@ document.addEventListener('alpine:init', () => {
                 id:'english-test',
                 title:'English Test ',
                 type_id:'android',
-                type_name:'Android Application',
+                type_name:'Android',
                 demo_url:'',
                 description:'blah test',
+                short_description: 'short',
                 technologies:['Laravel','Vue.js','MySQL','Ubuntu','Hostinger','Cloudflare','GithubActions'],
                 features:[
                     {
