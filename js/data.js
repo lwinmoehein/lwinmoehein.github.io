7106f1d8091c91a1488923769f8bad9d4296a3ad
lwinmoehein <dev.lwinmoehein@gmail.com>
2024-03-17 01:20:57 +0400

    - adjust project modal

--- js/data.js
+++ js/data.js
@@ -1,6 +1,8 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('utils', () => ({
         currentProjectDetail: null,
+        currentProjectFeature:null,
+        currentProjectFeatureIndex:0,
         projectDetails:[
             {
                 id:'english-daily',
@@ -8,19 +10,23 @@ document.addEventListener('alpine:init', () => {
                 type_id:'web',
                 type_name:'Web Application',
                 demo_url:'',
-                description:'blah',
+                description:'To make the images responsive and scale appropriately, we can set the maximum width of the images to 100% of their parent container, which in this case is .slide. Here\'s the updated SCSS code:\n',
+                technologies:['Laravel','Vue.js','MySQL','Ubuntu','Hostinger','Cloudflare','GithubActions'],
                 features:[
                     {
                         name:'Register',
-                        image_url:''
+                        image_url:'',
+                        description:'This is for registering shit'
                     },
                     {
                         name:'Login',
-                        image_url:''
+                        image_url:'',
+                        description:'This is for loggin in shit'
                     },
                     {
                         name:'Test',
-                        image_url:''
+                        image_url:'',
+                        description:'Sar U Yay Ngar'
                     }
                 ]
             },
@@ -31,21 +37,48 @@ document.addEventListener('alpine:init', () => {
                 type_name:'Android Application',
                 demo_url:'',
                 description:'blah test',
+                technologies:['Laravel','Vue.js','MySQL','Ubuntu','Hostinger','Cloudflare','GithubActions'],
                 features:[
                     {
                         name:'Hello',
-                        image_url:''
+                        image_url:'',
+                        description:'Test Desc'
                     },
                     {
                         name:'There',
-                        image_url:''
+                        image_url:'',
+                        description:'Test Desc'
                     },
                     {
                         name:'Test',
-                        image_url:''
+                        image_url:'',
+                        description:'Test Desc'
                     }
                 ]
             }
-        ]
+        ],
+        next() {
+            if(this.currentProjectFeatureIndex===this.currentProjectDetail.features.length-1){
+                this.currentProjectFeatureIndex = 0
+                this.currentProjectFeature = this.currentProjectDetail.features[0]
+                return
+            }
+            this.currentProjectFeatureIndex = this.currentProjectFeatureIndex+1
+            this.currentProjectFeature = this.currentProjectDetail.features[this.currentProjectFeatureIndex]
+        },
+        prev() {
+            if(this.currentProjectFeatureIndex===0){
+                this.currentProjectFeatureIndex = this.currentProjectDetail.features.length-1
+                this.currentProjectFeature = this.currentProjectDetail.features[this.currentProjectFeatureIndex]
+                return
+            }
+            this.currentProjectFeatureIndex = this.currentProjectFeatureIndex-1
+            this.currentProjectFeature = this.currentProjectDetail.features[this.currentProjectFeatureIndex]
+        },
+        onProjectClick(projectDetail){
+            this.currentProjectDetail = projectDetail
+            this.currentProjectFeatureIndex = 0
+            this.currentProjectFeature = projectDetail.features[0]
+        }
     }))
 })
